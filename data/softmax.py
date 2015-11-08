10f22363c47d242c22eef6ebd6a576b292ffcc97
Edward Banner <edward.banner@gmail.com>
2015-11-08 15:58:57 -0600

    Add data for vectorized softmax

--- data/softmax.py
+++ data/softmax.py
@@ -10,3 +10,6 @@ negatives = [-1]
 
 xs_train = positives + negatives
 ys_train = [1 for _ in range(m//2)] + [0 for _ in range(m//2)]
+
+X_train = np.array(xs_train[:]).reshape(1, m)
+ys_train = np.array(ys_train)
