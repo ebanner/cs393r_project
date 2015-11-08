e0f3e26f08549724d25590130f7c5f56b1ae19fa
Edward Banner <edward.banner@gmail.com>
2015-11-08 15:58:45 -0600

    Fix softmax bug

--- lib/softmax.py
+++ lib/softmax.py
@@ -9,3 +9,13 @@ def softmax(scores):
     e_x = np.exp(scores - scores.max())
     
     return e_x / e_x.sum()
+
+def softmax_vectorized(scores):
+    """Softmax function
+
+    Assumes the scores for each training example go vertically
+
+    """
+    e_x = np.exp(scores - scores.max(axis=0))
+
+    return e_x / e_x.sum(axis=0)
