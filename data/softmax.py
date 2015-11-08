ac21373031a85674b57a88c87202722e283d3bcc
Edward Banner <edward.banner@gmail.com>
2015-11-08 17:54:41 -0600

    Add xor fail

--- data/softmax.py
+++ data/softmax.py
@@ -2,14 +2,17 @@ import numpy as np
 
 m = 2
 
-# positives = [np.random.randn()+1 for _ in range(m//2)]
-# negatives = [np.random.randn()-1 for _ in range(m//2)]
-
+## 1D
 positives = [ 1]
 negatives = [-1]
 
 xs_train = positives + negatives
 ys_train = [1 for _ in range(m//2)] + [0 for _ in range(m//2)]
 
-X_train = np.array(xs_train[:]).reshape(1, m)
-ys_train = np.array(ys_train)
+## Vectorized
+
+positives = [[1,1], [2,2]]
+negatives = [[2,1], [1,2]]
+
+X_train = np.array(positives+negatives).T
+ys_train = [0, 0, 1, 1]
