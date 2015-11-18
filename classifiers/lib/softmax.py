d0ba45d1335f09b34509e1035a6f1cddea9e3d10
Edward Banner <edward.banner@gmail.com>
2015-11-18 12:09:41 -0600

    Restructure directory layout

---
+++ classifiers/lib/softmax.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def softmax_2d(scores):
+    """Compute the softmax between two numbers
+    
+    s1 is the number we're finding the softmax of
+    
+    """
+    e_x = np.exp(scores - scores.max())
+    
+    return e_x / e_x.sum()
+
+def softmax_vectorized(scores):
+    """Softmax function
+
+    Assumes the scores for each training example go vertically
+
+    """
+    e_x = np.exp(scores - scores.max(axis=0))
+
+    return e_x / e_x.sum(axis=0)
