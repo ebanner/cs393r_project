9f9c8d7c27df20e34f1ca4a6ee74c2edf4342e8d
Edward Banner <edward.banner@gmail.com>
2015-12-02 19:27:21 -0600

    Gradient checks pass

--- classifiers/data/rnn.py
+++ classifiers/data/rnn.py
@@ -1,14 +1,25 @@
+import numpy as np
+
 # Data for training rnn
 #
 # The idea here is that you should fire on a 2, but only if it's preceded by a
 # 1. Context matters!
 
 xs_train = [
-        ( 1, 1),
-        (-1, 1)
+    ( 1, 1),
+    (-1, 1)
 ]
 
 ys_train = [
-        (0, 0),
-        (1, 1)
+    (0, 0),
+    (1, 1)
 ]
+
+X_train = np.array([
+    [-1,1],
+    [-1,1]
+])
+
+Y_train = np.array(
+    [0, 1]
+)
