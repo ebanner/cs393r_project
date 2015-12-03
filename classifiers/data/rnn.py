9f159964e17c16f0a5ddb26bffd1a8ac451cf6ec
Edward Banner <edward.banner@gmail.com>
2015-12-02 21:00:12 -0600

    Make Karpathy's optimization more explicit

--- classifiers/data/rnn.py
+++ classifiers/data/rnn.py
@@ -15,11 +15,12 @@ ys_train = [
     (1, 1)
 ]
 
+# The goal here is to classify 1 always as 0, except when it's followed by a -1.
+# The class 2 is introduced as a special marker class
 X_train = np.array([
-    [-1,1],
-    [-1,1]
+    [1, 1, -1, 1],
 ])
 
 Y_train = np.array(
-    [0, 1]
+    [0, 0,  2, 1]
 )
